63db43aef210579a4a564f3f18c914efafd15ce4
Micha Alt <mail@lean-stack.de>
2021-05-04 22:55:46 +0200

    Bootstrap app.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 // App bootstrap
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
 // Global styles
 import './styles.css';
 
-console.log(`App started at ${new Date().toLocaleTimeString()}`);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => console.log(`App started at ${new Date().toLocaleTimeString()}`))
+  .catch(err => console.log(err));
