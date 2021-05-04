8587ad10e3eff7069177df5ae1f2b23625208715
Micha Alt <mail@lean-stack.de>
2021-05-04 22:44:09 +0200

    Add module declarations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,16 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
-@NgModule()
+@NgModule({
+  // Deklarationen dieses Moduls. Jede Komponente muss deklariert werden.
+  declarations: [
+    AppComponent
+  ],
+  // Module, deren Deklarationen importiert werden
+  imports: [
+  ],
+  // Exportierte Deklarationen, die anderen Modulen bereitgestellt werden
+  exports: [
+  ],
+})
 export class AppModule {}
